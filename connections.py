4a76c47598f5cb91e824f9c33c9bd764a1671915
NotMarek <46475336+NotMarek@users.noreply.github.com>
2020-03-06 22:22:48 +0100

    Rewrote for better readability

--- connections.py
+++ connections.py
@@ -1,55 +1,44 @@
-import requests, random, json, os, easygui
+import requests, random, json
 from colorama import init, Fore
-init(convert=True)
+init(convert=True, autoreset=True)
 
-pause = lambda : os.system('pause')
-clear = lambda : os.system("cls")
 
 def info():
-
     info = {}
-
-    print(f"{Fore.CYAN} Please input your discord tokens {Fore.RESET}")
+    print(f"{Fore.CYAN} Please input your discord tokens")
     info["token"] = str(input(" > "))
-    print(f"{Fore.CYAN} Please input your desired method: [battlenet, skype, lol, contacts] {Fore.RESET}")
+    print(f"{Fore.CYAN} Please input your desired method: [battlenet, skype, leagueoflegends, contacts]")
     info["option"] = str(input(" > "))
-    print(f"{Fore.CYAN} Input the amount of connections you'd like {Fore.RESET}")
-    info["amount"] = int(input(" > "))
-    print(f"{Fore.CYAN} Input the desired name {Fore.RESET}")
+    print(f"{Fore.CYAN} Input the amount of connections you'd like")
+    while 'amount' not in info:
+        try:
+            info["amount"] = int(input(" > "))
+        except ValueError:
+            print(f'{Fore.RED}That was not a number :( try again.')
+    print(f"{Fore.CYAN} Input the desired name")
     info["name"] = str(input(" > "))
-    return json.loads(str(info).replace("'", '"'))
+    return info
+
 
 def start():
-
     information = info()
+    for i in range(information["amount"]):
+        req = requests.put(f'https://discordapp.com/api/v6/users/@me/connections/{information["option"]}/{random.randint(1, 10)}',
+        json={
+            "name": information["name"],
+            "visibility": 1,
+            "verified": True
+            },
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": str(information["token"])
+            })
+        try:
+            if json.loads(req.text)["type"] == information["option"]:
+                print(f"{Fore.GREEN} Successfully added the new connection")
 
-    with requests.Session() as session:
-                i = 0
-                while True:
-                    i+=1
-                    if i < information["amount"]:
-                        req = session.put(f'https://discordapp.com/api/v6/users/@me/connections/{information["option"]}/{random.randint(1, 10)}', 
-
-                        json={
-                            "name": information["name"],
-                            "visibility": 1,
-                            "verified": True
-                            },
-                        headers={
-                            "Content-Type": "application/json",
-                            "Content-Length": str(len(information["name"])),
-                            "Authorization": str(information["token"])
-                            })
-
-                        try:
-                            if json.loads(req.text)["type"] == information["option"]:
-                                print(f"{Fore.GREEN} Successfully added the new connection {Fore.RESET}")
-                        except:
-                            print(f"{Fore.RED} An error occured. On token: {information['token']} {Fore.RESET}")   
-                    else:
-                        print(Fore.RED + "\n ")
-                        pause()
-                        exit()                                  
+        except KeyError:
+            print(f"{Fore.RED} An error occured. On token: {information['token']}")
 
 
 if __name__ == "__main__":
